8470dc71267568f6068f3d090898f7cf7a77de1b
dantheman3345 <u1022647@utah.edu>
2024-01-16 13:21:55 -0700

    my first commit!

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4vq2i_@c2k5r3_u-utgque#myu3jv-t12i577(f_vym*hol97a'
+SECRET_KEY = 'django-insecure-p0)pg&%2_qc6h=w$de)o)wz7h24c1783851z*)u_c$(=!2t1f$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
