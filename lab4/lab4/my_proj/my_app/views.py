7213297a8f42cdbf298cec0ed00bba8e52bd1169
Dantheman3345 <u1022647@utah.edu>
2024-02-07 11:56:13 -0700

    with changes I think

--- lab4/lab4/my_proj/my_app/views.py
+++ lab4/lab4/my_proj/my_app/views.py
@@ -1,7 +1,15 @@
 from django.views import View
 from django.shortcuts import render, redirect
 
+def title_name(the_name: str):
+    return the_name.lower()
+
+
 class HomePageView(View):
     def get(self, request):
-     
-        return render(request, 'my_app/index.html',{'hi':'hello world!'})
+
+        my_name = "DANIEL"
+        new_name = title_name(my_name)
+
+        the_context={{'hi':'hello class!','name':new_name}}
+        return render(request,' 'my_app/index.html',the_context)')
