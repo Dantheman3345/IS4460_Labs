836bf640d26ef9ae8b6c8e03e5738cb50b646f75
dantheman3345 <u1022647@utah.edu>
2024-03-19 09:22:49 -0600

    3/19 other

--- HW3_3/newMovie/MovieApp/scripts/get_test.py
+++ HW3_3/newMovie/MovieApp/scripts/get_test.py
@@ -1,6 +1,6 @@
 import requests
 import json
 
-#resp = requests.get('http://127.0.0.1:8000/api/movies/')
-#print(resp.status_code)  # This should print 200 for a successful request.
-#print(resp.text)  # This should print '[]' if there are no movies in your database yet.
+resp = requests.get('http://127.0.0.1:8000/api/movies/')
+print(resp.status_code)
+print(resp.text)
